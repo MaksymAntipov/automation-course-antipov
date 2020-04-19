30eec3fd53902c3498e5e1ff2acf593463593366
MaksymAntipov <antipovmaxdp@gmail.com>
2020-04-19 15:21:02 +0300

    First program

---
+++ src/main/java/org/example/lectures/lectures_01/First.java
@@ -0,0 +1,11 @@
+package org.example.lectures.lectures_01;
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world! ");
+        System.out.println("My name is Maksym");
+    }
+
+
+}
