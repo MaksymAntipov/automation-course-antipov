4e46ede81b74aa397d6be4a6665fda05ba149a7c
MaksymAntipov <antipovmaxdp@gmail.com>
2020-04-22 19:23:07 +0300

    new branch

---
+++ src/main/java/org/example/homeworks/hw_03/CheckOddEven.java
@@ -0,0 +1,16 @@
+package org.example.homeworks.hw_03;
+
+public class CheckOddEven {
+    public static void main(String[] args) {
+
+        int num = 97;
+
+        if (num % 2 == 0){
+                System.out.println("Четное");}
+            else{
+                System.out.println("Нечетное");
+            }
+        System.out.println("BYE");
+        }
+
+    }
---
+++ src/main/java/org/example/homeworks/hw_03/PrintNumberInWord.java
@@ -0,0 +1,15 @@
+package org.example.homeworks.hw_03;
+
+public class PrintNumberInWord {
+
+    public static void main(String[] args) {
+
+        int num = 13;
+        if (num==1);
+        System.out.println("");
+        {
+
+        }
+
+    }
+}
