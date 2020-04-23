8a3328e67157ec0c44dfc9dd1b891db34379ab39
MaksymAntipov <antipovmaxdp@gmail.com>
2020-04-23 13:49:36 +0300

    actualization

---
+++ src/main/java/org/example/homeworks/hw_03/Division_06.java
@@ -0,0 +1,11 @@
+package org.example.homeworks.hw_03;
+
+public class Division_06 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 2;
+
+        String number = x % y == 0 ? "Четное" : "Нечетное";
+
+    }
+}
--- src/main/java/org/example/homeworks/hw_03/FloatingPointDivision_08.java
+++ src/main/java/org/example/homeworks/hw_03/FloatingPointDivision_08.java
@@ -2,8 +2,15 @@ package org.example.homeworks.hw_03;
 
 public class FloatingPointDivision_08 {
     public static void main(String[] args) {
-        int y = 12;
-        int x = 23;
-    if ()
+        byte y = 3;
+        byte x = 5 ;
+    if (y>=x)
+    {
+        System.out.println(y+" имеет большое значение");
+    }
+    else  if (y<=x)
+    {
+        System.out.println(x+" имеет большее значение");
+    }
     }
 }
