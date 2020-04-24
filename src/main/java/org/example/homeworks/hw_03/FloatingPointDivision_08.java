c0faa981e8c598c32d2116067aa382a38847470c
MaksymAntipov <antipovmaxdp@gmail.com>
2020-04-24 21:47:12 +0300

    actualization

--- src/main/java/org/example/homeworks/hw_03/FloatingPointDivision_08.java
+++ src/main/java/org/example/homeworks/hw_03/FloatingPointDivision_08.java
@@ -1,16 +1,16 @@
 package org.example.homeworks.hw_03;
 
+import javax.swing.*;
+
 public class FloatingPointDivision_08 {
     public static void main(String[] args) {
-        byte y = 3;
-        byte x = 5 ;
-    if (y>=x)
-    {
-        System.out.println(y+" имеет большое значение");
-    }
-    else  if (y<=x)
-    {
-        System.out.println(x+" имеет большее значение");
-    }
+        double y = Math.abs(-8);
+        double x = Math.abs(9);
+
+        if (y >= x) {
+            System.out.println("Число y больше, равняется = " + y);
+        } else {
+            System.out.println("Число x больше, равняется = " + x);
+        }
     }
 }
---
+++ src/main/java/org/example/homeworks/hw_03/TheEquation_07.java
@@ -0,0 +1,9 @@
+package org.example.homeworks.hw_03;
+
+public class TheEquation_07 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+
+    }
+}
