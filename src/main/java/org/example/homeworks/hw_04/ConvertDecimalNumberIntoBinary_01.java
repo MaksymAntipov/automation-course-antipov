66cdc802b024fe77438dc4e4823d7354cba8fe31
MaksymAntipov <antipovmaxdp@gmail.com>
2020-04-26 12:38:55 +0300

    DZ

---
+++ src/main/java/org/example/homeworks/hw_04/ConvertDecimalNumberIntoBinary_01.java
@@ -0,0 +1,11 @@
+package org.example.homeworks.hw_04;
+
+public class ConvertDecimalNumberIntoBinary_01 {
+    public static void main(String[] args) {
+        int y = 4;
+
+        System.out.println(Integer.toBinaryString(y));
+    }
+}
+
+
