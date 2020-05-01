01b9dc6b618f7370d18706ef6b55bd5952199a26
MaksymAntipov <antipovmaxdp@gmail.com>
2020-05-01 14:15:14 +0300

    actualization

---
+++ src/main/java/org/example/homeworks/hw_05/TemperatureKC02.java
@@ -0,0 +1,18 @@
+package org.example.homeworks.hw_05;
+
+public class TemperatureKC02 {
+    public static void main(String[] args) {
+
+        double с = ConvertorKC(144);// ввести градусов в кельвинах
+        {
+            System.out.println("Градусов в цельсиях " + с);
+        }
+    }
+    public static double ConvertorKC(double k)
+    {
+        double c = k - 273.15;
+        return (double) c;
+    }
+}
+
+
