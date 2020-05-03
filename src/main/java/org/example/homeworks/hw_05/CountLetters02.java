5bba8d221dfd0bcdfbb82684ff1187263694d98b
MaksymAntipov <antipovmaxdp@gmail.com>
2020-05-03 13:08:59 +0300

    actualization

---
+++ src/main/java/org/example/homeworks/hw_05/CountLetters02.java
@@ -0,0 +1,12 @@
+package org.example.homeworks.hw_05;
+
+public class CountLetters02 {
+
+
+    public static void main(String[] args) {
+        String s1 = "Text TEXT texT";
+
+        System.out.println();
+
+    }
+}
