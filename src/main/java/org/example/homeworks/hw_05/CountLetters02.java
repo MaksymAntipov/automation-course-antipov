e6b00c1c6ff12a830fec893dee8b1be38a034ced
MaksymAntipov <antipovmaxdp@gmail.com>
2020-05-04 23:57:20 +0300

    actualization

--- src/main/java/org/example/homeworks/hw_05/CountLetters02.java
+++ src/main/java/org/example/homeworks/hw_05/CountLetters02.java
@@ -1,5 +1,6 @@
 package org.example.homeworks.hw_05;
 
+import javax.swing.*;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
@@ -8,6 +9,18 @@ public class CountLetters02 {
 
     public static void main(String[] args) {
 
-        String s1 = "Text TEXT texT";
-        
+        String s1 = "ttthgyrft";
+        char[] s2 = s1.toCharArray();
+        char x = 't';
+        int y = 0;
+
+        for (int i = 0; i<= s2.length-1; i++)
+
+            if(x == s2[i])
+            {
+                y+=1;
+                System.out.println("Символов "+ y);
+            }
+            else {
+                System.out.println("В тексте нет символа " + x);}
 }}
