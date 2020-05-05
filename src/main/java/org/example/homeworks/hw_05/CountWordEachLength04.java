b1b72f8110eb09f3994218d66296ed39123bfe32
MaksymAntipov <antipovmaxdp@gmail.com>
2020-05-05 12:59:11 +0300

    actualization

---
+++ src/main/java/org/example/homeworks/hw_05/CountWordEachLength04.java
@@ -0,0 +1,29 @@
+package org.example.homeworks.hw_05;
+
+public class CountWordEachLength04 {
+
+    public static void main(String[] args) {
+
+
+    String s1 = "eqwe qweqwe wqeq wq weqweqwe sdsd";{
+    char[] s2 = s1.toCharArray();
+    char x = ' ';
+    int y = 1; // это наверное как костыль какой-то? или и такой подход норм?
+
+        for (int i = 0; i<= s2.length-1; i++)
+
+
+            if(x == s2[i])
+    {
+        y+=1;
+    }
+    {
+        System.out.println("Слов в тексте "+ y);
+    }
+}}}
+
+
+
+
+
+
