5a1a040d65f4dfcb0dd25845d43ce1ccf682fdd6
MaksymAntipov <antipovmaxdp@gmail.com>
2020-05-09 12:36:41 +0300

    actualization

--- src/main/java/infrastructure/StringUtils.java
+++ src/main/java/infrastructure/StringUtils.java
@@ -1,14 +1,35 @@
 package infrastructure;
 
+import java.util.Random;
+
 public class StringUtils {
 
 
 
     public static String randomString(String type, int length) {
 
-        String rand = StringUtils.randomString("alpha", 15);
+        String alphabet;
 
-        return (String) rand;
+        switch (type)
+        {
+            case"Alpha": alphabet = "abcdefghijklmnopqrtuvwxyz";
+                break;
+            case "NUMERIC": alphabet = "0123456789";
+                break;
+            case "ALPHANUMERIC":
+                alphabet = "abcdefghijklmnopqrtuvwxyz0123456789";
+                break;
+            default: return null;
+        }
+
+        Random r = new Random();
+        String result = "";
+
+        for (int i = 0;i<length; i++) {
+            result += alphabet.charAt(r.nextInt(alphabet.length()));
+
+        }
+        return "result";
     }}
 
 
