919395b775b9789625e0d2cbbe42220b998b5634
MaksymAntipov <antipovmaxdp@gmail.com>
2020-05-11 17:35:40 +0300

    actualization

---
+++ src/main/java/org/example/homeworks/hw_07/AppTest.java
@@ -0,0 +1,13 @@
+package org.example.homeworks.hw_07;
+
+import java.sql.PreparedStatement;
+
+public class AppTest {
+
+    public static void main(String[] args) {
+
+        FractionNumbers test = new FractionNumbers(20, 5, '-');
+        System.out.println(test);
+    }}
+
+
