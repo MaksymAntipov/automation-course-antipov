f1ed8c1b5ac39d234c33eaeab661d754b89d6d17
MaksymAntipov <antipovmaxdp@gmail.com>
2020-05-11 23:24:53 +0300

    actualization

--- src/main/java/org/example/homeworks/hw_07/AppTest.java
+++ src/main/java/org/example/homeworks/hw_07/AppTest.java
@@ -2,12 +2,14 @@ package org.example.homeworks.hw_07;
 
 import java.sql.PreparedStatement;
 
+import static org.example.homeworks.hw_07.FractionNumbers.*;
+
 public class AppTest {
-
     public static void main(String[] args) {
-
-        FractionNumbers test = new FractionNumbers(9,1,'-');
-        System.out.println(test);
-    }}
-
+        double z = Partition(1,2);
+        {
+            System.out.println(z);
+        }
+    }
+}
 
--- src/main/java/org/example/homeworks/hw_07/FractionNumbers.java
+++ src/main/java/org/example/homeworks/hw_07/FractionNumbers.java
@@ -1,47 +1,30 @@
 package org.example.homeworks.hw_07;
 
 public class FractionNumbers {
-    double x;
-    double y;
-    char z;
 
-    public FractionNumbers(double x, double y, char z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    @Override
-    public String toString() {
-        return "FractionNumbers{" +
-                "x=" + x +
-                ", y=" + y +
-                ", z=" + z +
-                '}';
-    }
+public static double Plus(double x, double y)
+{
+    return x+y;
+}
+public  static double Minus(double x, double y)
+{
+    return x - y;
+}
 
-    public static double Calc(double x, double y, char z)
-    {
-        if (z == '-') {
-            double res = x - y;
-            return (double) res;
-        }
-        else if (z=='+')
-        {
-            double res = x+y;
-            return (double) res;
-        }
-        else if(z =='*') {
-            double res = x * y;
-            return (double) res;
-        }
-        else if (z == '/')
-        {
-            double res = x/y;
-            return (double) res;
-        }
-        else {
-            System.out.println("Такая операция не поддерживаетя");
-        }
-        return x; // эту строку рекомендовала внедрить java, только не могу понять, что я не так делал, зачем постоянно возвращать переменную x?
+public static double Multiply(double x, double y)
+{
+    return x*y;
+}
+public static double Partition(double x, double y)
+{
+    return x/y;
+}
+}
 
-    }}
+
+
+
+
+
+
+
